6b42ca95144878d422ba1c49f2e043622587bbe5
unknown <czesio517619032@wp.pl>
2019-05-15 16:56:48 +0200

    add simple vue for cart page

--- pages/cart.vue
+++ pages/cart.vue
@@ -4,12 +4,15 @@
     <div class="container">
       <b-button @click="clear()">clear</b-button>
       <div class="cartTable">
-        <p>{{cartValues}}</p>
+        <div class="cart" v-for="dog in dogs" :key="dog.id">
+            <img :src="`https://media.graphcms.com/${dog.image.handle}`" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import navBar from '@/components/navBar'
 export default {
   components: {
@@ -17,12 +20,41 @@ export default {
   },
   data () {
     return {
-      cartValues: ''
+      cartValues: '',
+      dogs: []
     }
   },
   mounted () {
     setTimeout ( () => {
       this.cartValues = this.$store.state.cart.cart
+      for(var counter = 0; this.cartValues.length > counter; counter++) {
+        console.log()
+        axios({
+  url: 'https://api-euwest.graphcms.com/v1/cjuv6vg2j85lu01fa1ppccsy7/master',
+  method: 'post',
+  data: {
+    query: `
+            {
+              dogs (where:{
+                id: "${this.cartValues[counter]}"
+              }) {
+                id,
+                name,
+                age,
+                description,
+                image {
+                  id,
+                  handle
+                }
+              }
+            }
+        `
+  }
+}).then((result) => {
+  this.dogs.push(result.data.data.dogs[0])
+  console.log(this.dogs)
+})
+      }
     },1000) 
   },
   methods: {
@@ -33,4 +65,11 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.cart {
+  img {
+    width:100px;
+  }
+}
+</style>
 
